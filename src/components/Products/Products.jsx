9491dd31856ee6c04480dc40f01951b7ce06c248
Roberto <r.apaza.cornejo@gmail.com>
2024-04-09 08:44:58 -0500

    Arreglando la seccion de productos

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { FaStar } from "react-icons/fa6";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
@@ -17,45 +17,55 @@ const Products = () => {
     fetchProducts();
   }, []);
 
- 
-
   return (
-    <div className="mt-14 mb-12">
+    <div className=" py-14 bg-gray-100">
       <div className="container">
         {/* header section */}
         <div className=" text-center mb-10 max-w[600px] mx-auto">
           <p className="text-sm text-solid-pink-600">Productos mas vendidos</p>
-          <h2 className="text-3xl font-bold" data-aos="fade-up">Productos</h2>
+          <h2 className="text-3xl font-bold" data-aos="fade-up">
+            Productos
+          </h2>
           <p className="text-xs text-gray-400" data-aos="fade-up">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit
           </p>
         </div>
         {/* body section */}
         <div>
-          <div  className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 place-items-center gap-5 ">
+          <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 place-items-center gap-5 ">
             {/* card section */}
             {products.map((data) => {
               return (
-                  <div key={data.id} className="space-y-3 bg-white p-4 rounded-md shadow-md shadow-solid-pink-200" data-aos="fade-up" data-aos-delay={data.aosDelay}>
-                    <img
-                      src={data.thumbnail}
-                      alt={data.title}
-                      className=" h-[200px] w-[150px] object-cover rounded-md"
-                    />
-                    <div>
-                      <h3 className="font-semibold">{data.title}</h3>
-                      <p className="text-sm text-gray-600">{data.description}</p>
-                      <div className="flex items-center gap-1">
-                        <FaStar className="text-solid-pink-600" />
-                        <span>{data.rating}</span>
-                      </div>
+                <div
+                  key={data.id}
+                  className="flex flex-col items-center gap-2 space-y-3 bg-white p-4 rounded-md shadow-md shadow-solid-pink-200"
+                  data-aos="fade-up"
+                  data-aos-delay={data.aosDelay}
+                >
+                  <img
+                    src={data.thumbnail}
+                    alt={data.title}
+                    className=" h-[200px] w-[150px] object-cover rounded-md"
+                  />
+                  <div>
+                    <h3 className="font-semibold">{data.title}</h3>
+                    <p className="text-sm text-gray-600">{data.description}</p>
+                    <div className="flex items-center gap-1">
+                      <FaStar className="text-solid-pink-600" />
+                      <span>{data.rating}</span>
                     </div>
                   </div>
+                </div>
               );
             })}
           </div>
           <div className="flex justify-center">
-            <Link to="/catalog" className="text-center mt-10 cursor-pointer bg-solid-pink-500 text-white py-1 p-2 rounded-md">Mirar todo los productos</Link>
+            <Link
+              to="/catalog"
+              className="text-center mt-10 cursor-pointer bg-solid-pink-500 text-white py-1 p-2 rounded-md"
+            >
+              Mirar todo los productos
+            </Link>
           </div>
         </div>
       </div>
