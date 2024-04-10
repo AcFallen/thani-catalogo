ca34bc7e1838e4cae8638484c00abdadab6fe0b5
Roberto Carlos Apaza Cornejo <r.apaza.cornejo@gmail.com>
2024-04-10 04:48:56 -0500

    Agregando barra lateral y contenido en la página de inicio de sesión

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,20 +1,32 @@
-import React from "react";
+import { useState } from "react";
+
 import Logo from "../assets/logo_tani.svg";
 import { RiStore2Line } from "react-icons/ri";
 import { RiServerLine } from "react-icons/ri";
-import { RiLogoutBoxLine } from "react-icons/ri";
+import { RiLogoutBoxLine, RiMenu3Fill, RiCloseLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
 
 const Dashboard = () => {
+  const [siderbar, setSidebar] = useState(false);
+
+  const handleSidebar = () => {
+    setSidebar(!siderbar);
+  }
+
   return (
-    <div className="min-h-screen grid grid-cols-6">
-      <div className="col-span-1 p-8">
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-6">
+      {/* siderbar */}
+      <div
+        className={`fixed lg:static w-[80%] md:w-[40%] lg:w-full top-0 z-50 bg-white transition-all ${
+          siderbar ? "-left-0" : "-left-full"
+        }   w-full h-screen overflow-y-hidden col-span-1 p-8 border shadow-md shadow-solid-pink-500`}
+      >
         {/* Logotipo */}
         <div className="text-center p-8">
           <h1 className="uppercase font-bold tracking[4px]">thani dashboard</h1>
         </div>
 
-        <div className=" bg-solid-pink-700 flex flex-col justify-between h-full">
+        <div className=" flex flex-col justify-between h-[500px]">
           {/* Menú */}
           <nav>
             <ul>
@@ -42,8 +54,18 @@ const Dashboard = () => {
           </div>
         </div>
       </div>
+      {/* botoon meno movil */}
+      <button onClick={handleSidebar} className="block lg:hidden absolute bottom-4 right-4 bg-solid-pink-700 p-2 text-white rounded-full text-2xl">
 
-      <div className="col-span-5"></div>
+        {siderbar ? <RiCloseLine /> : <RiMenu3Fill />}
+      </button>
+      {/* contenido */}
+      <div className="col-span-5">
+        {/* header */}
+        <header>
+          Hola mundo
+        </header>
+      </div>
     </div>
   );
 };
