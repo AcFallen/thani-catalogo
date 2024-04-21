99f4ef1dc0dee3d3636d500106d528f575a350dc
Roberto <r.apaza.cornejo@gmail.com>
2024-04-21 12:05:46 -0500

    Optimizaciond de Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,47 +1,62 @@
-import React from "react";
-import { Link , useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "../assets/logo_tani.svg";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { UserContext } from "../context/UserContext";
 
 const Login = () => {
+  const [form, setForm] = useState({ correo: "", password: "" });
+  const { storeUser } = useContext(UserContext);
 
-  const [correo , setCorreo] = useState('');
-  const [password , setPassword] = useState('');
-  const [error, setError] = useState('');
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const [error, setError] = useState("");
 
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-    
-    const response = await fetch('http://127.0.0.1:3000/login', {
-      method: 'POST',
+
+    const { correo, password } = form;
+
+    const response = await fetch("http://127.0.0.1:80/login", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ correo, password }),
-    })
+    });
 
     if (!response.ok) {
-      // Manejar error
-      console.error('Error en la respuesta de la API');
-      setError('Correo o contraseña incorrecta');
+      setError("Usuario o contraseña incorrectos");
+      setForm({
+        correo: "",
+        password: "",
+      });
+
       return;
     }
 
-    const data = await response.json();
+    const responseData = await response.json();
 
-    navigate('/dashboard');
-
-
-
-    // Actualizar el estado de la aplicación con los datos recibidos
-    console.log(data);
-  }
+    if (response.ok) {
+      storeUser(responseData.content);
+      navigate("/dashboard");
+    } else {
+      setError(responseData.message);
+    }
+  };
 
   return (
     <div className="flex items-center justify-center min-h-svh bg-solid-pink-200 px-4">
-      <form onSubmit={handleSubmit} className=" bg-slate-50 border-2 border-solid-pink-500 rounded-2xl p-8 sm:max-w-sm md:max-w-md md:max-h-max shadow-lg">
+      <form
+        onSubmit={handleLogin}
+        className=" bg-slate-50 border-2 border-solid-pink-500 rounded-2xl p-8 sm:max-w-sm md:max-w-md md:max-h-max shadow-lg"
+      >
         <Link to="/">
           <div className="drop-shadow-lg">
             <img
@@ -55,17 +70,21 @@ const Login = () => {
         <div className="flex flex-col gap-4">
           <h2 className="font-bold text-solid-pink-800">Correo</h2>
           <input
-            value={correo}
-            onChange={(e) => setCorreo(e.target.value)}
+            name="correo"
+            value={form.correo}
+            onChange={handleChange}
             className="border border-gray-200 rounded-md px-2 py-1 focus:outline-none focus:border focus:border-solid-pink-800"
-            type="text"
+            type="email"
+            required={true}
           />
           <h2 className="font-bold text-solid-pink-800">Contraseña</h2>
           <input
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+            value={form.password}
+            onChange={handleChange}
             className="border border-gray-200 rounded-md px-2 py-1 focus:outline-none focus:border focus:border-solid-pink-800"
             type="password"
+            required={true}
           />
           <button className=" hover:bg-solid-pink-200 bg-gradient-to-b from-solid-pink-700 to-solid-pink-800 py-2 rounded-md text-white font-bold ">
             Ingresar
