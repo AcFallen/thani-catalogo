4a9d2dd8cbb8292a4256f2bb0699f91197613975
Roberto <r.apaza.cornejo@gmail.com>
2024-04-21 12:04:17 -0500

    Protegi routas de dashboard

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -5,21 +5,27 @@ import Catalog from "../pages/Catalog";
 import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
 
-
 import "aos/dist/aos.css";
-import React from 'react';
+import React from "react";
+
+import { UserProvider } from "../context/UserContext";
+import  PrivateRouter  from "./PrivateRouter";
 
 const Router = () => {
-
   return (
-    <BrowserRouter>
+    <UserProvider>
+      <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/catalog" element={<Catalog />} />
-            <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/login" element={<Login />} />
+
+          <Route element={<PrivateRouter />}>
             <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
         </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
+    </UserProvider>
   );
 };
 
